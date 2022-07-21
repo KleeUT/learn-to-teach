18029a0dd4c62d534caf3ad15710cbdf377f8e54
Klee Thomas <klee.ut@gmail.com>
2022-07-21 12:01:04 +1000

    :sparkle: adding databse

--- functions/api/talkSubmission.ts
+++ functions/api/talkSubmission.ts
@@ -1,14 +1,15 @@
 import { PostSubmission, Submission } from "../../types";
 
-export const onRequestGet: PagesFunction = async () => {
-  const data: Submission[] = [
-    {
-      title: "title",
-      month: "jan",
-      speakerEmail: "bill@billy.com",
-      speakerName: "billy",
-    },
-  ];
+export const onRequestGet: PagesFunction<{
+  SPEAKER_SUBMIT_STORE: KVNamespace;
+}> = async ({ env }) => {
+  const itemPromise: Promise<Submission>[] = (
+    await env.SPEAKER_SUBMIT_STORE.list()
+  ).keys.map(async (key) => {
+    const item = await env.SPEAKER_SUBMIT_STORE.get(key.name);
+    return JSON.parse(item);
+  });
+  const data = await Promise.all(itemPromise);
   return new Response(
     JSON.stringify({
       data,
@@ -16,7 +17,11 @@ export const onRequestGet: PagesFunction = async () => {
   );
 };
 
-export const onRequestPost: PagesFunction = async ({ request }) => {
+export const onRequestPost: PagesFunction<{
+  SPEAKER_SUBMIT_STORE: KVNamespace;
+}> = async ({ request, env }) => {
+  const id = crypto.randomUUID();
   const body: PostSubmission = await request.json();
-  return new Response(JSON.stringify({ data: body.data }));
+  env.SPEAKER_SUBMIT_STORE.put(id, JSON.stringify({ id, ...body.data }));
+  return new Response(JSON.stringify({ message: "Stored", data: body.data }));
 };
